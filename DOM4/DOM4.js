c36d13a1ab4f11e96bd9824ca47ca971f056371b
Tamas Kovari <zakzag.dev@gmail.com>
2023-05-15 11:32:17 +0200

    DOM4 - only learning materials

---
+++ DOM4/DOM4.js
@@ -0,0 +1,16 @@
+/**
+ * DOM4 exercise is about Events and Event handling. You will read about Events on MDN and learn how to handle them,
+ * and you'll write a new application that uses different kind of Events like:
+ * - click, keyup, change, blur, focus, mouseover, mouseout, mouseenter, mouseleave, mousemove, mousedown, mouseup,
+ *   keydown, keyup, keypress
+ *
+ * Learning materials:
+ *  - Events on MDN:
+ *     https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events
+ *     https://developer.mozilla.org/en-US/docs/Web/Events
+ *     https://developer.mozilla.org/en-US/docs/Web/API/Event
+ *
+ *  - Event bubbling and capturing:
+ *   https://javascript.info/bubbling-and-capturing
+ *
+ */
