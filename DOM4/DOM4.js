36d8241b4b418ebdbfb27e9587d66cbef6c2558c
Tamas Kovari <zakzag.dev@gmail.com>
2023-05-15 11:44:12 +0200

    More materials  for DOM4

--- DOM4/DOM4.js
+++ DOM4/DOM4.js
@@ -1,16 +1,32 @@
 /**
- * DOM4 exercise is about Events and Event handling. You will read about Events on MDN and learn how to handle them,
- * and you'll write a new application that uses different kind of Events like:
+ * DOM4 exercise is about Events and Event handling and Forms. You will read about Events on MDN and learn how to handle
+ * them, and you'll write a new application that uses different kind of Events like:
  * - click, keyup, change, blur, focus, mouseover, mouseout, mouseenter, mouseleave, mousemove, mousedown, mouseup,
  *   keydown, keyup, keypress
  *
+ * - form events: submit, reset, change, input, focus, blur
+ * And also form elements like:
+ * - input, textarea, select, button, checkbox, radio, option
+ *
+ * The most important pages are marked with "!".
  * Learning materials:
+ *  - Forms on MDN:
+ *      https://developer.mozilla.org/en-US/docs/Learn/Forms
+ *    ! https://developer.mozilla.org/en-US/docs/Learn/Forms/Your_first_form
+ *      https://developer.mozilla.org/en-US/docs/Learn/Forms/How_to_structure_a_web_form
+ *    ! https://developer.mozilla.org/en-US/docs/Learn/Forms/Basic_native_form_controls
+ *    ! https://developer.mozilla.org/en-US/docs/Learn/Forms/HTML5_input_types
+ *      https://developer.mozilla.org/en-US/docs/Learn/Forms/Other_form_controls
+ *      https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation
+ *      https://developer.mozilla.org/en-US/docs/Learn/Forms/Styling_web_forms
+ *      https://developer.mozilla.org/en-US/docs/Learn/Forms/Advanced_form_styling
+ *
  *  - Events on MDN:
- *     https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events
+ *   ! https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events
  *     https://developer.mozilla.org/en-US/docs/Web/Events
  *     https://developer.mozilla.org/en-US/docs/Web/API/Event
  *
  *  - Event bubbling and capturing:
- *   https://javascript.info/bubbling-and-capturing
+ *   ! https://javascript.info/bubbling-and-capturing
  *
  */
