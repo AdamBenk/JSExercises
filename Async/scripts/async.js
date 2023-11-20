b95674311d0cf2f9cee436d174f52cf8053cc01a
Adam Benko <adam.benko.bp@gmail.com>
2023-11-20 19:05:28 +0100

    Exercise 1: 3 solutions(callbackhell, promise, async)

--- Async/scripts/async.js
+++ Async/scripts/async.js
@@ -1,18 +1,11 @@
 
 window.addEventListener("load", () => {    
     //moveDOMElement("picAnimated", {top:50, left:100}, {top:300, left:700}, 10);
-    moveDOMElementPromise("picAnimated", {top:50, left:100}, {top:300, left:700}, 10);
+    //moveDOMElementPromise("picAnimated", {top:50, left:100}, {top:300, left:700}, 10);
+    moveDOMElementAsync("picAnimated", {top:50, left:100}, {top:300, left:700}, 10);
 }); 
 
-/*function moveDOMElement(imagename, { top: a, left: b}, { top: aa, left: bb }, steps) {
-    findElement(imagename, (imageobject) => {
-        setStartPosition(imageobject, { top: a, left: b}, (imageobject) => {
-            setSteps(imageobject, { top: aa, left: bb }, steps, (imageobject, endcoordinates, steptop, stepleft) => {
-                makeMoves(imageobject, endcoordinates, steptop, stepleft);
-            });
-        });
-    });
-}*/
+
 
 
 
@@ -24,6 +17,15 @@ function moveDOMElementPromise(imagename, { top: a, left: b}, { top: aa, left: b
         .then((imageobject) => setSteps(imageobject, { top: aa, left: bb }, steps))
         .then((returnValues) => makeMoves(returnValues[0], returnValues[1], returnValues[2], returnValues[3]))
         .catch(() => console.error("error"));
+}
+
+async function moveDOMElementAsync(imagename, { top: a, left: b}, { top: aa, left: bb }, steps) {
+    let result1 = await findElement(imagename);
+    setStartPosition(result1, { top: a, left: b});
+    let result2 = await setStartPosition(result1, { top: a, left: b});
+    setSteps(result2, { top: aa, left: bb }, steps);
+    let result3 = await setSteps(result2, { top: aa, left: bb }, steps);
+    makeMoves(result3[0], result3[1], result3[2], result3[3]);
 }
 
 function findElement(imagename) {
@@ -48,7 +50,6 @@ function makeMoves(imageobject, endcoordinates, steptop, stepleft) {
     const moveIntervalID = setInterval(() => {
         const startTop = imageobject.offsetTop;
         const startLeft = imageobject.offsetLeft;
-        console.info("left and top:", startLeft, startTop, "endcoord:", endcoordinates.left, endcoordinates.top)
         imageobject.style.top = `${startTop + steptop}px`;
         imageobject.style.left = `${startLeft + stepleft}px`;
 
@@ -59,8 +60,18 @@ function makeMoves(imageobject, endcoordinates, steptop, stepleft) {
 }
 
 
+//CALLBACKHELL SOLUTION:
 
-/*function findElement(imagename, cb) {
+/*function moveDOMElement(imagename, { top: a, left: b}, { top: aa, left: bb }, steps) {
+    findElement(imagename, (imageobject) => {
+        setStartPosition(imageobject, { top: a, left: b}, (imageobject) => {
+            setSteps(imageobject, { top: aa, left: bb }, steps, (imageobject, endcoordinates, steptop, stepleft) => {
+                makeMoves(imageobject, endcoordinates, steptop, stepleft);
+            });
+        });
+    });
+
+function findElement(imagename, cb) {
     const imageobject = document.getElementById(`${imagename}`);
     cb(imageobject)
 }
