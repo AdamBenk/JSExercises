88e5e75f35a3dde0d9545731cea884a1e3a1b957
Tamas Kovari <zakzag.dev@gmail.com>
2023-11-23 23:05:07 +0100

    some fixes for async move img

--- Async/scripts/async.js
+++ Async/scripts/async.js
@@ -1,29 +1,35 @@
 
 window.addEventListener("load", () => {    
-    //moveDOMElement("picAnimated", {top:50, left:100}, {top:300, left:700}, 10);
+    //moveDOMElementPromise("picAnimated", {top:50, left:100}, {top:300, left:700}, 100);
     //moveDOMElementPromise("picAnimated", {top:50, left:50}, {top:500, left:100}, 10);
     //moveDOMElementAsync("picAnimated", {top:50, left:100}, {top:300, left:700}, 4);
-    moveAroundCorners({top:0, left:0}, 700, 700, 4);
+    moveAroundCorners({top:4, left:4}, 700, 700, 20);
 }); 
 
 
 async function moveAroundCorners(startposition, topstep, leftstep, innersteps) {
-    const mov1 = await moveDOMElementPromise("picAnimated", startposition, {top:topstep, left:startposition.left}, innersteps);
-    console.info("mov1:", mov1)
-    const mov2 = await moveDOMElementPromise("picAnimated", mov1, {top: mov1.top, left:mov1.left + leftstep}, innersteps);
-    console.info("mov2:", mov2)
-    const mov3 = await moveDOMElementPromise("picAnimated", mov2, {top: mov2.top - topstep, left:mov2.left}, innersteps);
-    console.info("mov3:", mov3)
-    moveDOMElementPromise("picAnimated", mov3, startposition, innersteps);
+    const img1ID = "picAnimated";
+    const img2ID = "picAnimated2";
+
+    const p1 = startposition;
+    const p2 = {left: startposition.left, top: startposition.top + topstep};
+    const p3 = {left: startposition.left + leftstep, top: startposition.top + topstep};
+    const p4 = {left: startposition.left + leftstep, top: startposition.top};
+
+    await moveDOMElementPromise(img1ID, p1, p2, innersteps);
+    await moveDOMElementPromise(img1ID, p1, p3, innersteps);
+    await moveDOMElementPromise(img1ID, p3, p4, innersteps);
+    await moveDOMElementPromise(img1ID, p4, p1, innersteps);
 }
 
-function moveDOMElementPromise(imagename, { top: a, left: b}, { top: aa, left: bb }, steps) {
+function moveDOMElementPromise(imagename, startPos, endPos, steps) {
     return new Promise((resolve) => {
-            resolve(findElement(imagename))
-        })
-        .then((imageobject) => setStartPosition(imageobject, { top: a, left: b}))
-        .then((imageobject) => setSteps(imageobject, { top: aa, left: bb }, steps))
-        .then((returnValues) => makeMoves(returnValues[0], returnValues[1], returnValues[2], returnValues[3]))
+        let imageobject = findElement(imagename);
+
+        setStartPosition(imageobject, startPos)
+        const stepValue = setSteps(startPos, endPos, steps);
+        makeMoves(imageobject, startPos, endPos, stepValue, resolve);
+    });
 }
 
 async function moveDOMElementAsync(imagename, { top: a, left: b}, { top: aa, left: bb }, steps) {
@@ -32,11 +38,11 @@ async function moveDOMElementAsync(imagename, { top: a, left: b}, { top: aa, lef
     let result2 = setStartPosition(result1, { top: a, left: b});
     setSteps(result2, { top: aa, left: bb }, steps);
     let result3 = setSteps(result2, { top: aa, left: bb }, steps);
-    makeMoves(result3[0], result3[1], result3[2], result3[3]);
+    makeMoves(imageobject, startPos, endPos, stepValue, resolve);
 }
 
 function findElement(imagename) {
-    const imageobject = document.getElementById(`${imagename}`);
+    const imageobject = document.querySelector(`#${imagename}`);
     return imageobject;
 }
 
@@ -46,24 +52,33 @@ function setStartPosition(imageobject, startcoordinates) {
     return imageobject;
 }
 
-function setSteps(imageobject, endcoordinates, steps) {
-    const steptop = (endcoordinates.top - imageobject.offsetTop)/steps;
-    const stepleft = (endcoordinates.left - imageobject.offsetLeft)/steps;
-    const returnValues = [imageobject, endcoordinates, steptop, stepleft];
-    return returnValues;
+function setSteps(startPos, endPos, steps) {
+    const stepTop = (endPos.top - startPos.top) / steps;
+    const stepLeft = (endPos.left - startPos.left) / steps;
+    return  {
+        top: stepTop,
+        left: stepLeft
+    };
 }
 
-function makeMoves(imageobject, endcoordinates, steptop, stepleft, cb) {
+function makeMoves(imageobject, startPos, endPos, stepValue, cb) {
+    let currPos = startPos;
     const moveIntervalID = setInterval(() => {
-        const startTop = imageobject.offsetTop;
-        const startLeft = imageobject.offsetLeft;
-        imageobject.style.top = `${startTop + steptop}px`;
-        imageobject.style.left = `${startLeft + stepleft}px`;
-        if (imageobject.offsetLeft === endcoordinates.left && imageobject.offsetTop === endcoordinates.top) {
+
+        currPos.left  += stepValue.left;
+        currPos.top  += stepValue.top;
+        console.info("coords:",currPos.left, currPos.top, stepValue.left, stepValue.top);
+        imageobject.style.left = `${currPos.left}px`;
+        imageobject.style.top = `${currPos.top}px`;
+
+        const diffX = Math.abs(currPos.left - endPos.left);
+        const diffY = Math.abs(currPos.top - endPos.top);
+        if (diffX < 0.0000001 && diffY < 0.0000001) {
             clearInterval(moveIntervalID);
+            cb();
         }
-    }, 500)
-    return endcoordinates;
+    }, 50);
+    return endPos;
 }
 
 //CALLBACKHELL SOLUTION:
