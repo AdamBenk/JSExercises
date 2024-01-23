bbd3093ab200212ff57f66e798f37a18284f29f1
Tamas Kovari <zakzag.dev@gmail.com>
2024-01-24 00:31:10 +0100

    weather initial commit

---
+++ Weather/webpack.config.js
@@ -0,0 +1,53 @@
+import miniCss from "mini-css-extract-plugin";
+import browserSyncPlugin from 'browser-sync-webpack-plugin';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+    mode: 'development',
+    entry: {
+        "index": [
+            "./Weather/scripts/async.js",
+            "./Weather/css/styles.scss",
+        ],
+    },
+    output: {
+        path: __dirname + "/dist/js/",
+        filename: "[name].min.js",
+        publicPath: "/dist/js/",
+        hashFunction: "xxhash64"
+    },
+    module: {
+        rules: [{
+            test: /\.(s*)css$/,
+            use: [
+                {
+                    loader: miniCss.loader,
+                    options: {
+                        publicPath: '/1/'
+                    }
+                },
+                'css-loader',
+                'sass-loader',
+            ]
+        }]
+    },
+    plugins: [
+        new miniCss({
+            filename: "[name].min.css"
+        }),
+        new browserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+
+            files: ['*.html', '*.js', '*.scss', '*.json'], // => files to watch
+            server: {
+                baseDir: [__dirname]
+            },
+        })
+    ],
+    watch: true
+};
