cde7c05e168dbc2cd6524b2ce79f9b750c6bd8aa
Tamas Kovari <zakzag.dev@gmail.com>
2024-02-02 18:03:06 +0100

    task 2 : components

---
+++ Weather/src/HourlyDisplay.js
@@ -0,0 +1,15 @@
+class HourlyDisplay {
+    time;
+    weather;
+    temp;
+    constructor(hourlyDisplayData) {
+        this.time = hourlyDisplayData.time;
+        this.weather = hourlyDisplayData.weather;
+        this.temp = hourlyDisplayData.temp;
+    }
+
+    render() {
+
+    }
+
+}
